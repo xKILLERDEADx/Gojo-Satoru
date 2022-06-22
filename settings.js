6c6710a75895a5c732f1254beb3bc921ce9b682d
Muhammad Abid <36359688+xKILLERDEADx@users.noreply.github.com>
2022-06-22 18:53:51 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': '0320ba195a50', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
